b1cf7a734d0f6162c67d152b2ba0d31a43cf7a9d
Phellipe Sander <phellipe.sander1@gmail.com>
2020-11-21 23:17:03 -0300

    Feign clients

--- hr-payroll/src/main/java/com/mscourse/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/mscourse/hrpayroll/services/PaymentService.java
@@ -2,29 +2,35 @@ package com.mscourse.hrpayroll.services;
 
 import com.mscourse.hrpayroll.entities.Payment;
 import com.mscourse.hrpayroll.entities.Worker;
+import com.mscourse.hrpayroll.feignClients.WorkerFeignClients;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class PaymentService {
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
+    //Utilizando RestTemplate
 
+//    @Value("${hr-worker.host}")
+//    private String workerHost;
+
+//    @Autowired
+//    private RestTemplate restTemplate;
+
+//        public Payment getPayment(long workerId, int days){
+//        Map<String, String> uriVariables = new HashMap<>();
+//        uriVariables.put("id", String.valueOf(workerId));
+//        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+//
+//        return new Payment(worker.getName(), worker.getDailyIncome(), days);
+//    }
+
+    //Utilizando Feign
     @Autowired
-    private RestTemplate restTemplate;
-
+    private WorkerFeignClients workerFeignClients;
 
     public Payment getPayment(long workerId, int days){
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", String.valueOf(workerId));
-        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
-
+        Worker worker = workerFeignClients.findById(workerId).getBody();
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 
