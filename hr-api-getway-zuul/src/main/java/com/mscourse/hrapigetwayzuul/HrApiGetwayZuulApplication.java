9dca1378a20bfebb622ac0f27bfd828148e16af5
Phellipe Sander <phellipe.sander1@gmail.com>
2020-11-23 21:38:16 -0300

    Project hr-api-getway-zuul created

---
+++ hr-api-getway-zuul/src/main/java/com/mscourse/hrapigetwayzuul/HrApiGetwayZuulApplication.java
@@ -0,0 +1,13 @@
+package com.mscourse.hrapigetwayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrApiGetwayZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrApiGetwayZuulApplication.class, args);
+    }
+
+}
