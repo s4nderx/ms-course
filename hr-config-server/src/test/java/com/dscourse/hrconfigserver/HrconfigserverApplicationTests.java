6c7697b36e4d5219a47560e726a3d89eedbb9037
s4nderx <phellipe.sander1@gmail.com>
2021-04-07 23:49:59 -0300

    hr-config-server created

---
+++ hr-config-server/src/test/java/com/dscourse/hrconfigserver/HrconfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dscourse.hrconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrconfigserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
